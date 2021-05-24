ee7a4655aeea933ffcb5d81508a171b89b447441
Rob Findley <rfindley@google.com>
2021-05-24 19:50:15 -0400

    internal/testenv: update linux-arm to linux-arm-scaleway
    CL 303230 renamed the linux-arm builder to linux-arm-scaleway, but the
    x/tools ExitIfSmallMachine guard was not updated accordingly, resulting
    in consistent failures for some tests.
    
    For golang/go#45931
    For golang/go#46183
    Updates golang/go#32834
    
    Change-Id: Ief5f17fc61cb38cf6b7cf63b6cd5e64f9d56261e
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/322409
    Trust: Robert Findley <rfindley@google.com>
    Run-TryBot: Robert Findley <rfindley@google.com>
    gopls-CI: kokoro <noreply+kokoro@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Rebecca Stambler <rstambler@golang.org>

--- internal/testenv/testenv.go
+++ internal/testenv/testenv.go
@@ -182,8 +182,9 @@ func NeedsTool(t Testing, tool string) {
 // It should be called from within a TestMain function.
 func ExitIfSmallMachine() {
 	switch os.Getenv("GO_BUILDER_NAME") {
-	case "linux-arm":
-		fmt.Fprintln(os.Stderr, "skipping test: linux-arm builder lacks sufficient memory (https://golang.org/issue/32834)")
+	case "linux-arm-scaleway":
+		// "linux-arm" was renamed to "linux-arm-scaleway" in CL 303230.
+		fmt.Fprintln(os.Stderr, "skipping test: linux-arm-scaleway builder lacks sufficient memory (https://golang.org/issue/32834)")
 		os.Exit(0)
 	case "plan9-arm":
 		fmt.Fprintln(os.Stderr, "skipping test: plan9-arm builder lacks sufficient memory (https://golang.org/issue/38772)")
