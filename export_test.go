42cc9bb7f275af2860d3186d8afafd462487f517
Masaya Hayashi <mcdonalds.only@gmail.com>
2022-08-20 23:37:09 +0900

    test: add export_test.go to test gosimports.Process

---
+++ export_test.go
@@ -0,0 +1,53 @@
+package gosimports_test
+
+import (
+	"bytes"
+	"os"
+	"testing"
+
+	"github.com/rinchsan/gosimports"
+)
+
+func TestMain(m *testing.M) {
+	gosimports.Debug = true
+	m.Run()
+}
+
+func TestProcess_from_src(t *testing.T) {
+	src, err := os.ReadFile("export.go")
+	if err != nil {
+		t.Fatal("expected: err == nil")
+	}
+
+	formatted, err := gosimports.Process("", src, nil)
+	if err != nil {
+		t.Fatal("expected: err == nil")
+	}
+
+	if !bytes.Equal(src, formatted) {
+		t.Fatal("expected: src == formatted")
+	}
+}
+
+func TestProcess_from_filename(t *testing.T) {
+	formatted, err := gosimports.Process("export.go", nil, nil)
+	if err != nil {
+		t.Fatal("expected: err == nil")
+	}
+
+	src, err := os.ReadFile("export.go")
+	if err != nil {
+		t.Fatal("expected: err == nil")
+	}
+
+	if !bytes.Equal(src, formatted) {
+		t.Fatal("expected: src == formatted")
+	}
+}
+
+func TestProcess_unknown_filename(t *testing.T) {
+	_, err := gosimports.Process("unknown.go", nil, nil)
+	if err == nil {
+		t.Fatal("expected: err != nil")
+	}
+}
